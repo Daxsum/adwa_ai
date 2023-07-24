747667420c66451b25d8fdd836a76b54a5e1ed00
Kalab Tenadeg <daxum@192.168.1.16>
2023-07-24 16:38:57 +0300

    feat:autentication and autorization using @clerk

--- app/(Dashboard)/(routes)/dashboard/page.tsx
+++ app/(Dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,11 @@
 
+import { UserButton } from "@clerk/nextjs"
 export default function Home() {
+
   return (
    <div >
     dashboard page (protected)
+<UserButton afterSignOutUrl="/"/>
    </div>
   )
 }
